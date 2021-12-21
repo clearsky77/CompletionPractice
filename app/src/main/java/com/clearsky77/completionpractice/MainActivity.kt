ee87261e1a78882f3a87be93423e0ee4b57222ea
clearsky77 <clearsky3264@gmail.com>
2021-12-21 22:36:11 +0900

    테스트 기본 환경 세팅

--- app/src/main/java/com/clearsky77/completionpractice/MainActivity.kt
+++ app/src/main/java/com/clearsky77/completionpractice/MainActivity.kt
@@ -2,10 +2,40 @@ package com.clearsky77.completionpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val TAG: String = "로그"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupEvents()
+        setValues()
+
+
+    }
+
+    private fun setupEvents() {
+        btnExplosion.setOnClickListener {
+            callBackMethod()
+        }
+    }
+
+    private fun setValues() {
+    }
+
+    fun callBackMethod(){
+        Log.d(TAG, "callBackMaethod()")
+
+        Handler().postDelayed({
+            Log.d(TAG, "3초 후에 폭발")
+            txtView.visibility = View.VISIBLE
+        }, 3000)
     }
 }
