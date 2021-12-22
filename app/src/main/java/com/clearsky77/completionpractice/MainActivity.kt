b59a1218592061f07096489b016ab06ac620d975
clearsky77 <clearsky3264@gmail.com>
2021-12-22 12:37:59 +0900

    시간 조정이 가능하도록 함. (순서 이해를 돕기 위해)  - InputMethodManager 사용  - Log.d 도 추가

--- app/src/main/java/com/clearsky77/completionpractice/MainActivity.kt
+++ app/src/main/java/com/clearsky77/completionpractice/MainActivity.kt
@@ -5,15 +5,22 @@ import android.os.Bundle
 import android.os.Handler
 import android.util.Log
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     val TAG: String = "로그"
+    var millisec = 0L
+
+    var imm: InputMethodManager? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 입력받는 방법을 관리하는 Manager객체를  요청하여 InputMethodmanager에 반환한다.
+        imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager?
 
         setupEvents()
         setValues()
@@ -23,9 +30,14 @@ class MainActivity : AppCompatActivity() {
 
     private fun setupEvents() {
         btnSetOff.setOnClickListener {
+
+            imm?.hideSoftInputFromWindow(btnSetOff.getWindowToken(), 0) // 키보드를 사라지게 함
+
+            millisec = (edtSec.text.toString().toInt() * 1000).toLong()
+
             callBackMethod1(completion = {
                 Log.d(TAG, "2초 후에 폭발했다!")
-                btnSetOff.visibility = View.GONE
+                laySetOffSetting.visibility = View.GONE
                 txtView.visibility = View.VISIBLE
                 txtMessage.text = it
 
@@ -55,7 +67,7 @@ class MainActivity : AppCompatActivity() {
         btnReplay.setOnClickListener {
             txtView.visibility = View.GONE
             btnReplay.visibility = View.GONE
-            btnSetOff.visibility = View.VISIBLE
+            laySetOffSetting.visibility = View.VISIBLE
             txtMessage.text = ""
         }
 
@@ -68,35 +80,40 @@ class MainActivity : AppCompatActivity() {
     fun callBackMethod1(completion: (String) -> Unit){
         Log.d(TAG, "callBackMaethod1()")
         Handler().postDelayed({
+            Log.d(TAG, "callBackMaethod1() 펑! -----")
             completion("callBackMethod1에서 펑!🎆")
-        }, 2000)
+        }, millisec)
     }
 
     fun callBackMethod2(completion: (String) -> Unit){
         Log.d(TAG, "callBackMaethod2()")
         Handler().postDelayed({
+            Log.d(TAG, "callBackMaethod2() 펑! -----")
             completion("    callBackMethod2에서도 펑!\uD83C\uDF86")
-        }, 1000)
+        }, millisec)
     }
 
     fun callBackMethod3(completion: (String) -> Unit){
         Log.d(TAG, "callBackMaethod3()")
         Handler().postDelayed({
+            Log.d(TAG, "callBackMaethod3() 펑! -----")
             completion("        callBackMethod3에서도 펑!\uD83C\uDF86")
-        }, 1000)
+        }, millisec)
     }
 
     fun callBackMethod4(completion: (String) -> Unit){
         Log.d(TAG, "callBackMaethod4()")
         Handler().postDelayed({
+            Log.d(TAG, "callBackMaethod4() 펑! -----")
             completion("            callBackMethod4에서도 펑!\uD83C\uDF86")
-        }, 1000)
+        }, millisec)
     }
 
     fun callBackMethod5(completion: (String) -> Unit){
         Log.d(TAG, "callBackMaethod5()")
         Handler().postDelayed({
+        Log.d(TAG, "callBackMaethod5() 펑! -----")
             completion("                callBackMethod5에서도 펑!\uD83C\uDF86 \n 콜백 헬이 완성 되었다 -_-)v")
-        }, 1000)
+        }, millisec)
     }
 }
