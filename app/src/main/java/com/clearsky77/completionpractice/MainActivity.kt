c9bd36ade29eeea543cf03e888211269af0c995e
clearsky77 <clearsky3264@gmail.com>
2021-12-22 11:23:23 +0900

    순차적으로 실행 되도록 활용해 보았다.

--- app/src/main/java/com/clearsky77/completionpractice/MainActivity.kt
+++ app/src/main/java/com/clearsky77/completionpractice/MainActivity.kt
@@ -22,14 +22,42 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setupEvents() {
-        btnExplosion.setOnClickListener {
+        btnSetOff.setOnClickListener {
             callBackMethod1(completion = {
                 Log.d(TAG, "2초 후에 폭발했다!")
+                btnSetOff.visibility = View.GONE
                 txtView.visibility = View.VISIBLE
                 txtMessage.text = it
+
+                callBackMethod2(completion = {
+                    txtMessage.text = txtMessage.text.toString()+"\n"+it
+
+                    callBackMethod3(completion = {
+                        txtMessage.text = txtMessage.text.toString() + "\n" + it
+
+                        callBackMethod4(completion = {
+                            txtMessage.text = txtMessage.text.toString() + "\n" + it
+
+                            callBackMethod5(completion = {
+                                txtMessage.text = txtMessage.text.toString() + "\n" + it
+                                btnReplay.visibility = View.VISIBLE
+                            })
+
+                        })
+
+                    })
+
+                })
+
             })
         }
 
+        btnReplay.setOnClickListener {
+            txtView.visibility = View.GONE
+            btnReplay.visibility = View.GONE
+            btnSetOff.visibility = View.VISIBLE
+            txtMessage.text = ""
+        }
 
 
     }
@@ -38,10 +66,37 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun callBackMethod1(completion: (String) -> Unit){
-        Log.d(TAG, "callBackMaethod()")
-
+        Log.d(TAG, "callBackMaethod1()")
         Handler().postDelayed({
-            completion("callBackMethod1에서 폭발했다.")
+            completion("callBackMethod1에서 펑!🎆")
         }, 2000)
+    }
+
+    fun callBackMethod2(completion: (String) -> Unit){
+        Log.d(TAG, "callBackMaethod2()")
+        Handler().postDelayed({
+            completion("    callBackMethod2에서도 펑!\uD83C\uDF86")
+        }, 1000)
+    }
+
+    fun callBackMethod3(completion: (String) -> Unit){
+        Log.d(TAG, "callBackMaethod3()")
+        Handler().postDelayed({
+            completion("        callBackMethod3에서도 펑!\uD83C\uDF86")
+        }, 1000)
+    }
+
+    fun callBackMethod4(completion: (String) -> Unit){
+        Log.d(TAG, "callBackMaethod4()")
+        Handler().postDelayed({
+            completion("            callBackMethod4에서도 펑!\uD83C\uDF86")
+        }, 1000)
+    }
+
+    fun callBackMethod5(completion: (String) -> Unit){
+        Log.d(TAG, "callBackMaethod5()")
+        Handler().postDelayed({
+            completion("                callBackMethod5에서도 펑!\uD83C\uDF86 \n 콜백 헬이 완성 되었다 -_-)v")
+        }, 1000)
     }
 }
